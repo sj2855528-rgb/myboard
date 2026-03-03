1e5fd46a752a28ebaf33baf15bd33ffaaf0cfbd6
sj2855528@gmail.com <sj2855528@gmail.com>
2026-03-04 01:05:02 +0900

    init project myboard

---
+++ src/main/java/servlets/TestServlet.java
@@ -0,0 +1,20 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		System.out.println("TestServlet.....");
+		response.getWriter().print("<html><head><title>Test Page</title></head><body><h1>Test</h1></body></html>");
+	}
+
+}
